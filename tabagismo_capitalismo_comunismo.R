f01fe6254db3ea1be2c730bea7d78a2e26c9e6e6
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-05 19:49:05 -0300

    Add files via upload

---
+++ tabagismo_capitalismo_comunismo.R
@@ -0,0 +1,23 @@
+
+# Tabagismo em países capitalistas e comunistas --------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 05/10/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/smoking -------------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Tabagismo é um dos maiores problemas de saúde no mundo. Milhões de pessoas
+### vivem em condições precárias de saúde por causa do tabagismo e pesquisadores
+### estimam que a cada ano cerca de 8 milhões de pessoas morrem prematuramente
+### devido ao tabagismo.
+
+### Tabagismo tem sido o principal problema de saúde por muitas décadas. Por todo
+### o século 20 é estimado que cerca de 100 milhões de pessoas morreram prematuramente
+### por causa do tabagismo, a maioria delas em países mais ricos.
+
+### A porcentagem de fumantes na população mundial está caindo e devido ao tabagismo
+### ser um grande problema de saúde hoje, isto é um dos avanços mais positivos na
+### saúde global. Isso torna possível que milhões de pessoas possam viver uma vida
+### mais longa e mais saudável.
+
+
