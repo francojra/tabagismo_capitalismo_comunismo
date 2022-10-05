ad2c56c4223cb4782fb33d2786733add3abe8d57
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-05 20:14:55 -0300

    Add files via upload

--- tabagismo_capitalismo_comunismo.R
+++ tabagismo_capitalismo_comunismo.R
@@ -34,4 +34,44 @@ names(smok)
 
 # Manipular dados --------------------------------------------------------------------------------------------------------------------------
 
+smok <- smok %>%
+  select(-Code) %>%
+  rename(por_tabag = Deaths...Cause..All.causes...Risk..Smoking...OWID...Sex..Both...Age..Age.standardized..Percent.) %>%
+  view()
+
+smok1 <- smok %>%
+  filter(Entity %in% c("United States", "Germany", "Japan",
+                       "China", "Cuba", "Noth Korea")) %>%
+  group_by(Entity) %>%
+  summarise(media = mean(por_tabag),
+            sd = sd(por_tabag), n = n(),
+            se = sd/sqrt(n)) %>%
+  view()
+
+smok2 <- smok %>%
+  filter(Entity %in% c("United States", "Germany", "Japan",
+                       "China", "Cuba", "Noth Korea")) %>%
+  view()
+
+# Gráficos ---------------------------------------------------------------------------------------------------------------------------------
+
+c4a("safe", 6)
+
+ggplot(smok1, aes(x = fct_reorder(Entity, media), 
+                  y = media, fill = Entity)) +
+  geom_col(width = 0.9) +
+  geom_errorbar(aes(ymin = media - se, ymax = media + se),
+                size = 0.8, width = 0.2) +
+  scale_fill_manual(values = c("#88CCEE", "#CC6677",
+                               "#DDCC77", "#117733",
+                                "#332288", "#AA4499")) +
+  scale_y_continuous(expand = expansion(mult = c(0,0))) +
+  labs(x = "Países", y = "Porcentagem de mortes 
+                         atribuídas ao tabagismo") +
+  theme_ipsum(axis_title_size = 16,
+              axis_text_size = 14) +
+  theme(legend.position = "none",
+        axis.text = element_text(color = "black"))
+
+
 
